ed60415765cc374bb6ccb8bc4da992705d19c0ad
adityasanil <aditya.sanil@somaiya.edu>
2017-07-22 13:55:57 +0530

    select department

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <?php
 
 ?>
-<<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>main page</title>
+    <title>Select department</title>
+    <link rel="stylesheet" href="layout.css">
 </head>
 <body>
-    <p>Admin login</p>
+    <center>
+    <div class="box">
+        <!--    redirect to data entry page-->
+
+        <button class="buttonsLayout"><a href="">Computers</a></button>
+        <br><br>
+        <button class="buttonsLayout"><a href="">IT</a></button>
+        <br><br>
+        <button class="buttonsLayout"><a href="">ETRX</a></button>
+        <br><br>
+        <button class="buttonsLayout"><a href="">EXTC</a></button>
+
+    </div>
+    </center>
 </body>
 </html>
